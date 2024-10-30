916f5fe03e31161a412cf86e18b1e24390b3cda5
RoopashreeHT <BHAL1KOR@bosch.com>
2024-10-31 00:53:07 +0530

    Update TemperatureBreachChecker.cpp

--- TemperatureBreachChecker.cpp
+++ TemperatureBreachChecker.cpp
@@ -10,8 +10,6 @@ CoolingTypeLimit coolingTypeLimits[NUM_COOLING_TYPES] = {
 
 BreachType ClassifyTemperatureBreach(CoolingType coolingType, double temperatureInC) {
    printf("cooling type = %d, coolingTypeLimits[coolingType].highLimit= %f", coolingType,coolingTypeLimits[coolingType].highLimit);
-    printf("Comparing to %f, expe%f ",lowerLimit, value);
-  printf("Comparing to h %f, expe%f ",upperLimit, value);
   return (temperatureInC < coolingTypeLimits[coolingType].lowLimit) ? TOO_LOW : (temperatureInC > coolingTypeLimits[coolingType].highLimit) ? TOO_HIGH : NORMAL;
   
 }
