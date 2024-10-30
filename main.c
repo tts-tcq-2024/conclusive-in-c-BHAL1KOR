dd43c1162df14b2df3690dfde8b705c25315c568
RoopashreeHT <BHAL1KOR@bosch.com>
2024-10-30 20:02:32 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "temperature_breach.h"
+#include "alert.h"
+
+void CheckAndAlert(CoolingType coolingType, double temperatureInC, bool hasController) {
+  BreachType breachType = ClassifyTemperatureBreach(coolingType, temperatureInC);
+  if (hasController) {
+    SendToController(breachType);
+  } else {
+    SendToEmail(breachType);
+  }
+}
+
+int main() {
+  CheckAndAlert(PassiveCooling, 50, true);
+  CheckAndAlert(HiActiveCooling, 50, false);
+  CheckAndAlert(MedActiveCooling, 50, true);
+  return 0;
+}
