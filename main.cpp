9ab31adf41611b4fa592e5db3f1fc4c3073ab4ff
RoopashreeHT <BHAL1KOR@bosch.com>
2024-10-31 00:56:01 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@
 void CheckAndTemperatureBreachCheckAndAlert(CoolingType coolingType, double temperatureInC, bool hasController) {
   BreachType breachType = ClassifyTemperatureBreach(coolingType, temperatureInC);
    if (breachType != NORMAL) {
-    printf("%d--%f***%d",coolingType, temperatureInC,breachType);
     void (*alertFunction)(BreachType) = hasController ? SendToController : SendToEmail;
     alertFunction(breachType);
   }
