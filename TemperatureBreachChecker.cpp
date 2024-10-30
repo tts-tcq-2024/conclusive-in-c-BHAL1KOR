6158e3a549ca910f75068760a1965b14eb1417af
RoopashreeHT <BHAL1KOR@bosch.com>
2024-10-30 23:43:21 +0530

    Update TemperatureBreachChecker.cpp

--- TemperatureBreachChecker.cpp
+++ TemperatureBreachChecker.cpp
@@ -6,7 +6,7 @@ BreachType InferBreach(double value, int lowerLimit, int upperLimit) {
 }
 
 BreachType ClassifyTemperatureBreach(CoolingType coolingType, double temperatureInC) {
-  printf("here got %d, %d", coolingType, temperatureInC);
+  printf("here got %d, %f", coolingType, temperatureInC);
   for (CoolingType coolingType = PASSIVE_COOLING; coolingType <= MED_ACTIVE_COOLING; coolingType = (CoolingType)(coolingType + 1)) {
     printf("got output %d\n', InferBreach(temperatureInC, coolingTypeLimits[coolingType].lowLimit, coolingTypeLimits[coolingType].highLimit));
     return InferBreach(temperatureInC, coolingTypeLimits[coolingType].lowLimit, coolingTypeLimits[coolingType].highLimit);
