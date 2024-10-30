69a405af239683d0e90dcea3b9a572a482ed4436
RoopashreeHT <BHAL1KOR@bosch.com>
2024-10-30 21:22:21 +0530

    Update BatteryTemperatureMonitorTests.c

--- BatteryTemperatureMonitorTests.c
+++ BatteryTemperatureMonitorTests.c
@@ -2,42 +2,25 @@
 #include "TemperatureBreachAlerter.h"
 #include <gtest/gtest.h>
 
-TEST(TemperatureBreach, InfersBreachAsPerLimits) {
-  ASSERT_EQ(InferBreach(12, 20, 30), TooLow);
-  ASSERT_EQ(InferBreach(35, 20, 30), TooHigh);
-  ASSERT_EQ(InferBreach(25, 20, 30), Normal);
+void TestBreachCheckAndAlert(bool hasController, CoolingType coolingType, double temperature, const std::string& expectedOutput) {
+    testing::internal::CaptureStdout();
+    CheckAndTemperatureBreachCheckAndAlert(coolingType, temperature, hasController);
+    std::string output = testing::internal::GetCapturedStdout();
+    ASSERT_EQ(output, expectedOutput);
 }
 
-TEST(TemperatureBreach, ClassifiesTemperatureBreach) {
-  ASSERT_EQ(ClassifyTemperatureBreach(PassiveCooling, 50), TooHigh);
-  ASSERT_EQ(ClassifyTemperatureBreach(HiActiveCooling, 50), Normal);
-  ASSERT_EQ(ClassifyTemperatureBreach(MedActiveCooling, 50), TooHigh);
-}
+TEST(TemperatureBreach, ControllerAlerts) {
+    bool hasController = true;
+    for (CoolingType coolingType = PASSIVE_COOLING; coolingType <= MED_ACTIVE_COOLING; ++coolingType) {
+        TestBreachCheckAndAlert(hasController, coolingType, coolingTypeLimits[coolingType].highLimit, ""); // Normal
+        TestBreachCheckAndAlert(hasController, coolingType, coolingTypeLimits[coolingType].lowLimit - 1, "feed : 1\n"); // TooLow
+        TestBreachCheckAndAlert(hasController, coolingType, coolingTypeLimits[coolingType].highLimit + 1, "feed : 2\n"); // TooHigh
+    }
 
-TEST(Alert, SendsToController) {
-  testing::internal::CaptureStdout();
-  SendToController(TooLow);
-  std::string output = testing::internal::GetCapturedStdout();
-  ASSERT_EQ(output, "feed : 1\n");
-}
-
-TEST(Alert, SendsToEmail) {
-  testing::internal::CaptureStdout();
-  SendToEmail(TooLow);
-  std::string output = testing::internal::GetCapturedStdout();
-  ASSERT_EQ(output, "To: a.b@c.com\nHi, the temperature is too low\n");
-}
-
-TEST(Main, CheckAndAlertWithController) {
-  testing::internal::CaptureStdout();
-  CheckAndAlert(PassiveCooling, 50, true);
-  std::string output = testing::internal::GetCapturedStdout();
-  ASSERT_EQ(output, "feed : 2\n");
-}
-
-TEST(Main, CheckAndAlertWithoutController) {
-  testing::internal::CaptureStdout();
-  CheckAndAlert(HiActiveCooling, 50, false);
-  std::string output = testing::internal::GetCapturedStdout();
-  ASSERT_EQ(output, "To: a.b@c.com\nHi, the temperature is too high\n");
+    hasController = false;
+    for (CoolingType coolingType = PASSIVE_COOLING; coolingType <= MED_ACTIVE_COOLING; ++coolingType) {
+        TestBreachCheckAndAlert(hasController, coolingType, coolingTypeLimits[coolingType].highLimit, ""); // Normal
+        TestBreachCheckAndAlert(hasController, coolingType, coolingTypeLimits[coolingType].lowLimit - 1, "To: a.b@c.com\nHi, the temperature is too low\n"); // TooLow
+        TestBreachCheckAndAlert(hasController, coolingType, coolingTypeLimits[coolingType].highLimit + 1, "To: a.b@c.com\nHi, the temperature is too high\n"); // TooHigh
+    }
 }
