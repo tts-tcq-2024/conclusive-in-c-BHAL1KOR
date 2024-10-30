a9a343bb8181e16de24d65382bf2eb37437f2a4b
RoopashreeHT <BHAL1KOR@bosch.com>
2024-10-30 22:39:39 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 #include "TemperatureBreachAlerter.h"
 #include <stdbool.h>
 
+CoolingTypeLimits coolingTypeLimits[] = {
+    {PASSIVE_COOLING, PASSIVE_COOLING_LOW, PASSIVE_COOLING_HIGH},
+    {HI_ACTIVE_COOLING, HI_ACTIVE_COOLING_LOW, HI_ACTIVE_COOLING_HIGH},
+    {MED_ACTIVE_COOLING, MED_ACTIVE_COOLING_LOW, MED_ACTIVE_COOLING_HIGH}
+};
+
 void CheckAndTemperatureBreachCheckAndAlert(CoolingType coolingType, double temperatureInC, bool hasController) {
   BreachType breachType = ClassifyTemperatureBreach(coolingType, temperatureInC);
   void (*alertFunction)(BreachType) = hasController ? SendToController : SendToEmail;
