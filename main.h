6fd32e07e5c4ca6b0a8f5ca174894eba4884fba5
RoopashreeHT <BHAL1KOR@bosch.com>
2024-10-30 22:29:41 +0530

    Create main.h

---
+++ main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "TemperatureBreachChecker.h" // Include the necessary header for CoolingType
+
+void CheckAndTemperatureBreachCheckAndAlert(CoolingType coolingType, double temperatureInC, bool hasController);
+
+#endif // MAIN_H
