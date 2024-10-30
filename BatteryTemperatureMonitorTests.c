12c9a6facb5083fc0c69af8d2b948be2fdf88e13
RoopashreeHT <BHAL1KOR@bosch.com>
2024-10-30 22:07:19 +0530

    Update BatteryTemperatureMonitorTests.c

--- BatteryTemperatureMonitorTests.c
+++ BatteryTemperatureMonitorTests.c
@@ -13,8 +13,8 @@ void TestBreachCheckAndAlert(bool hasController, CoolingType coolingType, double
 TEST(TemperatureBreach, Alerts) {
     for (bool hasController : {true, false}) {
         for (CoolingType coolingType = PASSIVE_COOLING; coolingType <= MED_ACTIVE_COOLING; ++coolingType) {
-            std::string tooLowAlert = hasController ? "feed : 1\n" : "To: a.b@c.com\nHi, the temperature is too low\n";
-            std::string tooHighAlert = hasController ? "feed : 2\n" : "To: a.b@c.com\nHi, the temperature is too high\n";
+            const char* tooLowAlert = hasController ? "feed : 1\n" : "To: a.b@c.com\nHi, the temperature is too low\n";
+            const char* tooHighAlert = hasController ? "feed : 2\n" : "To: a.b@c.com\nHi, the temperature is too high\n";
 
             TestBreachCheckAndAlert(hasController, coolingType, coolingTypeLimits[coolingType].highLimit, ""); // Normal
             TestBreachCheckAndAlert(hasController, coolingType, coolingTypeLimits[coolingType].lowLimit - 1, tooLowAlert); // TooLow
