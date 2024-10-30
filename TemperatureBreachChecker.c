14336faff42505de079d6f8658b342a4ae0417c2
RoopashreeHT <BHAL1KOR@bosch.com>
2024-10-30 22:11:20 +0530

    Update TemperatureBreachChecker.c

--- TemperatureBreachChecker.c
+++ TemperatureBreachChecker.c
@@ -1,15 +1,15 @@
 #include "TemperatureBreachChecker.h"
 
+BreachType InferBreach(double value, int lowerLimit, int upperLimit) {
+  return (value < lowerLimit) ? TOO_LOW : (value > upperLimit) ? TOO_HIGH : NORMAL;
+}
+
 BreachType ClassifyTemperatureBreach(CoolingType coolingType, double temperatureInC) {
-  for (int i = 0; i < sizeof(coolingTypeLimits) / sizeof(coolingTypeLimits[0]); i++) {
-    if (coolingTypeLimits[i].coolingType == coolingType) {
-      return InferBreach(temperatureInC, coolingTypeLimits[i].lowerLimit, coolingTypeLimits[i].upperLimit);
+    for (int i = 0; i < sizeof(coolingTypeLimits) / sizeof(coolingTypeLimits[0]); ++i) {
+        if (coolingTypeLimits[i].type == coolingType) {
+      return InferBreach(temperatureInC, coolingTypeLimits[i].lowLimit, coolingTypeLimits[i].highLimit);
     }
   }
   // Handle unexpected coolingType value
   return NORMAL;
-}
-
-BreachType InferBreach(double value, int lowerLimit, int upperLimit) {
-  return (value < lowerLimit) ? TOO_LOW : (value > upperLimit) ? TOO_HIGH : NORMAL;
 }
